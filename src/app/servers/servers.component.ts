5bfbfdea98ec3b51aae5e84716c2311167a6c32b
Alessandro <romano.alessandro.1996@gmail.com>
2023-02-15 12:57:03 +0100

    Utilizzo dell'Event Binding + passaggio ed utilizzo dei dati con Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,11 +14,26 @@ import { Component } from '@angular/core';
 
 export class ServersComponent {
   allowNewServer = false;
+  ServerCreationStatus = 'No server was created!'
+  serverName = '';
 
   constructor(){
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
+
+
+  ngOnInit(){
+
+  }
+
+onCreateServer() {
+  this.ServerCreationStatus = 'Server was created!'
 }
 
+onUpdateServerName(event: any){
+  this.serverName = (<HTMLInputElement>event.target).value;
+}
+
+}

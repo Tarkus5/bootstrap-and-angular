c24e518d31329ea7214ea0d46ab2e51ae708c08e
Alessandro <romano.alessandro.1996@gmail.com>
2023-02-15 12:31:20 +0100

    Uso del Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,12 +4,21 @@ import { Component } from '@angular/core';
   //selector: '[app-servers]', abbiamo selezionato per elemento, aggiungendo le quadre
   //selector: '.app-servers', abbiamo selezionato per classe
   selector: 'app-servers', //Da utilizzare praticamente sempre questa scrittura
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`, //Con le backtick ( alt + 96) possiamo scrivere il codice su più linee
+  //template: `
+  //<app-server></app-server>
+  //<app-server></app-server>`, //Con le backtick ( alt + 96) possiamo scrivere il codice su più linee
   //All'interno del componente è NECESSARIO avere un template (o un templateUrl se prendiamo il codice HTML da un file esterno)
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
 
+export class ServersComponent {
+  allowNewServer = false;
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
+
